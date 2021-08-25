478810354abd94d485c0c1c43c499bab2e9f41ed
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-08-25 13:40:25 -0300

    NuGet

--- ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -1,4 +1,5 @@
-﻿using ByteBank.Modelos;
+﻿using Humanizer;
+using ByteBank.Modelos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +13,24 @@ namespace ByteBank.SistemaAgencia
         static void Main(string[] args)
         {
 
-            ContaCorrente conta = new ContaCorrente(847, 489754);
-            
-            Console.WriteLine(conta.Numero);
+            //ContaCorrente conta = new ContaCorrente(847, 489754);
+
+            //  Console.WriteLine(conta.Numero);
+
+            TesteDePacoteDoNuget();
 
             Console.ReadLine();
+        }
+
+        public static void TesteDePacoteDoNuget()
+        {
+            DateTime dataFimPagamento = new DateTime(2021, 8, 22);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+            Console.WriteLine(mensagem);
         }
     }
 }
